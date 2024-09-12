21a777feecdd9b6e26bca1bc67c983d726921a31
dasom222g <dasom228@gmail.com>
2024-09-12 18:29:17 +0900

    Add components

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const InputField = () => {
-  return <input type="text" name="email" placeholder="Email" />;
+  return (
+    <input
+      type="text"
+      name="email"
+      placeholder="Email"
+      className="block rounded-lg border border-gray-800"
+    />
+  );
 };
 
 export default InputField;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import InputField from "../components/InputField";
+import LoginButton from "../components/LoginButton";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   // logic
@@ -19,9 +21,24 @@ const Login = () => {
       {/* <Link to={"/"} style={{ color: "red" }} className="link">
         Home화면으로 이동
       </Link> */}
-      <h3>Churead에서 소통해보세요</h3>
-      <InputField />
-      <InputField />
+      <h1>
+        <img src="./images/logo.svg" alt="churead로고" />
+      </h1>
+      <h3 className="text-red">Churead에서 소통해보세요</h3>
+      {/* START: 폼 영역 */}
+      <form id="login-form">
+        <InputField />
+        <InputField />
+        <LoginButton />
+      </form>
+      {/* END: 폼 영역 */}
+      <div>
+        <p>계정이 없으신가요?</p>
+        <Link>가입하기</Link>
+      </div>
+      {/* START: 소셜 로그인 영역 */}
+      <LoginButton />
+      {/* END: 소셜 로그인 영역 */}
     </div>
   );
 };
