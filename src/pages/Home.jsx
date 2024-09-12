05745847e61009c4bec6ca2b3e7c386b08514105
dasom222g <dasom228@gmail.com>
2024-09-12 14:56:06 +0900

    Add component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div style={{ backgroundColor: "red" }}>Home</div>;
+  return <div className="text-red-500">Home</div>;
 };
 
 export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import InputField from "../components/InputField";
 
 const Login = () => {
-  return <div style={{ backgroundColor: "blue" }}>Login</div>;
+  // logic
+  // const history = useNavigate();
+
+  // const goToHome = () => {
+  //   history("/");
+  // };
+
+  // view
+  return (
+    <div>
+      {/* <h2>Login</h2> */}
+      {/* <button type="button" onClick={goToHome}>
+        Home화면으로 이동
+      </button> */}
+      {/* <Link to={"/"} style={{ color: "red" }} className="link">
+        Home화면으로 이동
+      </Link> */}
+      <h3>Churead에서 소통해보세요</h3>
+      <InputField />
+      <InputField />
+    </div>
+  );
 };
 
 export default Login;
