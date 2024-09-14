b22a437c04a6334e7495321f778d07bc3b936bb0
dasom222g <dasom228@gmail.com>
2024-09-14 16:55:45 +0900

    Add link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 py-2">
-      <img src="./images/logo.svg" className="mx-auto h-16" alt="로고" />
-    </div>
+    <h1 className="fixed top-0 left-0 right-0 py-2">
+      <Link to="/">
+        <img src="./images/logo.svg" className="mx-auto h-16" alt="로고" />
+      </Link>
+    </h1>
   );
 };
 
