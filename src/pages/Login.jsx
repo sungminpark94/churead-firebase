ce6499f8103a977f0ce99233df1b721d1b60da6d
dasom222g <dasom228@gmail.com>
2024-09-19 11:56:43 +0900

    Add form data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InputField from "../components/InputField";
 import LoginButton from "../components/LoginButton";
 import { Link } from "react-router-dom";
@@ -10,6 +10,40 @@ const Login = () => {
   // const goToHome = () => {
   //   history("/");
   // };
+
+  // 자식으로부터 받은 inputValue의 값을 state에 저장
+  /**
+   * 1. handleInputChange함수에서 data라는 inputValue와 field라는 해당 input의 필드값을 받아온다.
+   * 2. 새로운 변수 선언하여 inputValue의 값을 넣어준다.(객체형태)
+   * 3. setFormData를 이용해서 새로운 변수의 값으로 변경한다.
+   */
+
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (inputValue, field) => {
+    // field: 'email', 'password'
+    const newFormData = { ...formData, [field]: inputValue };
+    setFormData(newFormData);
+
+    // if (field === "email") {
+    //   newFormData = { ...formData, email: inputValue };
+    // } else {
+    //   newFormData = { ...formData, password: inputValue };
+    // }
+    // setFormData(newFormData);
+    // console.log("🚀", field, inputValue);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("🚀 FormData:", formData);
+  };
 
   // view
   return (
@@ -29,9 +63,17 @@ const Login = () => {
           Churead에서 소통해보세요
         </h3>
         {/* START: 폼 영역 */}
-        <form id="login-form" className="text-center flex flex-col gap-2">
-          <InputField type="text" field="email" />
-          <InputField type="password" field="password" />
+        <form
+          id="login-form"
+          className="text-center flex flex-col gap-2"
+          onSubmit={handleLogin}
+        >
+          <InputField type="text" field="email" onChange={handleInputChange} />
+          <InputField
+            type="password"
+            field="password"
+            onChange={handleInputChange}
+          />
           <LoginButton category="login" text="Login" />
         </form>
         {/* END: 폼 영역 */}
