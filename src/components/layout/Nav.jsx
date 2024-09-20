4638f1a9204f81dd5d2787d1e3d7a1ea52f48314
dasom222g <dasom228@gmail.com>
2024-09-20 12:02:40 +0900

    Add pathname check

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { RiEditBoxFill, RiHome5Fill, RiHome5Line } from "react-icons/ri";
 // import { RiHome5Fill } from "react-icons/ri";
 import { RiEditBoxLine } from "react-icons/ri";
@@ -16,6 +16,15 @@ const Nav = () => {
    * 4. html부분에서 li태그를 navList의 map을 이용해서 반복 생성함
    * 5. li태그에 key라는 prop에 요소의 id값 연결
    * 6. html요소에 속성(to, icon)을 navList의 각 객체의 값으로 연결
+   */
+
+  const location = useLocation(); // 현재 머무르는 페이지 정보 객체
+  /**
+   * 사용자가 현재 머무르는 페이지에 따라 nav 아이콘 활성환 아이콘으로 변경
+   * 1. 사용자가 현재 머무르는 페이지의 pathname 가져오기
+   * 2. 해당 pathname과 map을 돌리는 nav라는 요소의 pathname이 같은지 체크
+   * 3. 체크한 값이 true면 activeIcon으로 변경
+   * 4. 체크한 값이 false면 icon으로 보여주기
    */
 
   const navList = [
@@ -46,7 +55,8 @@ const Nav = () => {
         {navList.map((nav) => (
           <li key={nav.id}>
             <Link to={nav.pathname} className="block p-6">
-              {nav.icon}
+              {/* 사용자가 현재 머무르는 페이지 pathname과 요소의 pathname 같인지 체크하여 activeIcon 보여주기 */}
+              {location.pathname === nav.pathname ? nav.activeIcon : nav.icon}
             </Link>
           </li>
         ))}
