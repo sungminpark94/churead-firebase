c0c4ec1d61a4cfc5e21358f2c2a25e56cd0f7da4
dasom222g <dasom228@gmail.com>
2024-09-23 11:11:04 +0900

    Add delete logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,28 @@ const Home = ({ churead }) => {
 
   console.log("churead", churead);
 
+  /**
+   * 아이템 삭제하기
+   * 1. 휴지통 아이콘이 있는 버튼을 클릭한다
+   * 2. 클릭 이벤트가 발생한다.
+   * 3. 클릭 이벤트가 발생시 handleDelete라는 함수가 호출된다.
+   * 4. handleDelete 내부 논리
+   * 4-1. confirm창을 띄운다.
+   * 4-2. 사용자 선택한 값(boolean타입)을 ok라는 변수에 저장한다.
+   * 4-3. 사용자 선택한 값이 true이면 onDelete라는 이벤트를 호출한다.
+   * 4-4. onDelete라는 이벤트에서 선택된 아이템 즉 data를 인자에 넣어서 부모에게 올려준다.
+   * 5. 부모는 onDelete라는 이벤트에 handleDelete라는 함수를 연결한다.
+   * 6. feedList에 filter함수를 사용한다.
+   * 6-1. filter함수에서 각 요소들의 id값과 자식으로부터 받아온 인자아이템의 id값과 비교해서 일치하지 않는 요소들만 뽑아낸다.
+   * 7. filter함수로 리턴받은 배열을 feedList라는 state에 반영한다.
+   */
+
+  const handleDelete = (selectedItem) => {
+    const filterList = feedList.filter((item) => item.id !== selectedItem.id);
+    console.log("🚀 ~ filterList:", filterList);
+    setFeedList(filterList);
+  };
+
   // 진입시 딱 한번 실행
   useEffect(() => {
     if (!churead) return;
@@ -37,7 +59,7 @@ const Home = ({ churead }) => {
           {/* START: 피드 영역 */}
           <ul>
             {feedList.map((feed) => (
-              <FeedItem key={feed.id} data={feed} />
+              <FeedItem key={feed.id} data={feed} onDelete={handleDelete} />
             ))}
           </ul>
           {/* END: 피드 영역 */}
